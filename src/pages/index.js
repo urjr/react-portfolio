3f6b4716fc4636705e2524738914727af571a475
Ulises Reyes <ulisesreyes@ulisess-mbp.lan>
2022-03-01 17:09:09 -0500

    added about and 404 pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
-import React from "react"
+import React from 'react'
 
 export default function Home() {
-  return <div>Hello world!</div>
+    return (
+        <section>
+            <h1>Hi, I'm Ulises, a UX Designer and Illustrator based in Brooklyn, New York</h1>
+            <h3>Currently, I design Marketing Tools for Google. Formerly at Notarize, Broadlume, and Smarking</h3>
+        </section>
+    )
 }
