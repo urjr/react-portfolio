4d377766eb838d7ecfc05a897153999ae3e3ccfe
Ulises Reyes <urjr@gmail.com>
2022-03-04 15:30:12 -0500

    first graphql query for site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,4 +7,9 @@
 module.exports = {
   /* Your site config here */
   plugins: [`gatsby-plugin-sass`],
+  siteMetadata: {
+    title: 'Ulises Reyes | UX Designer',
+    description: 'Ulises is a UX designer and Illustrator currently Working as an Interactoin designer for Google. Before Google, he has worked at Notarize, Broadlume (formerly AdHawk, TechStars Boulder 2015), and Smarking (Y Combinator W14)',
+    copyright: 'Copyright 2022'
+  },
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,14 @@
-import React, {useState} from 'react'
+import { graphql } from 'gatsby';
+import React from 'react'
 import Layout from '../layouts/layout'
 import * as componentStyles from '../styles/main.module.scss'
 
-export default function Home() {
-
+export default function Home({ data }) {
+   
     // Playing with state and click events
-    const [name, setName] = useState('Original');
-    console.log(name);
+    console.log(data);
 
-    const handleClick = () => {
-        if (name !== 'Original') {
-            setName('Original');
-        } else {
-            setName('Replacement');
-        }
-        console.log(name);
-    };
-
+    const { description, title, copyright } = data.site.siteMetadata;
     return (
         <Layout>
             <section className={componentStyles.header}>
@@ -24,9 +16,23 @@ export default function Home() {
                     <h1>Hi, I'm Ulises, a UX Designer and Illustrator based in Brooklyn, New York</h1>
                     <h3>Currently, I design Marketing Tools for Google. Formerly at Notarize, Broadlume, and Smarking</h3>
                 </div>
+                <p>{ copyright }</p>
+                <p>{ title }</p>
+                <p>{ description }</p>
             </section>
-            <button onClick={() => { handleClick() }}>{ name }</button>
         </Layout>
 
     )
 }
+
+export const query = graphql`
+    query siteInfo {
+        site {
+        siteMetadata {
+            description
+            title
+            copyright
+        }
+        }
+    }  
+`
