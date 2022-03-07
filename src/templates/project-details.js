c01b5d4ad2f53099b0cadb598783a6228ce3795f
Ulises Reyes <urjr@gmail.com>
2022-03-07 10:49:45 -0500

    added content to project post content

--- src/templates/project-details.js
+++ src/templates/project-details.js
@@ -1,13 +1,33 @@
+import { graphql } from 'gatsby'
 import React from 'react'
 import Layout from '../layouts/Layout'
 
-export default function projectDetails() {
+export default function projectDetails({ data }) {
+    const { html } = data.markdownRemark
+    const { title, name, description } = data.markdownRemark.frontmatter
+
   return (
     <Layout>
-        <h2>title</h2>
+        <h2>{ title }</h2>
+        <h2>{ name }</h2>
+        <h2>{ description }</h2>
         <div className='content'>
-            {/* <div dangerouslySetInnerHTML={} /> */}
+            <div dangerouslySetInnerHTML={{ __html: html }} />
         </div>
     </Layout>
   )
 }
+
+export const query = graphql`
+    query Projects($slug: String) {
+        markdownRemark(frontmatter: {slug: {eq: $slug}}) {
+        frontmatter {
+            title
+            slug
+            name
+            description 
+        }
+        html
+        }
+    }
+`
