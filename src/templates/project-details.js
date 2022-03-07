fa2a198d3962d9d29e82e07128bbfaa73100dd87
Ulises Reyes <urjr@gmail.com>
2022-03-06 19:28:39 -0500

    added project template, made links open in project pages

---
+++ src/templates/project-details.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../layouts/Layout'
+
+export default function projectDetails() {
+  return (
+    <Layout>
+        <h2>title</h2>
+        <div className='content'>
+            {/* <div dangerouslySetInnerHTML={} /> */}
+        </div>
+    </Layout>
+  )
+}
