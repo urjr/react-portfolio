f918e59cc6b2e1b6eb22a342d20c88b616bea326
Ulises Reyes <urjr@gmail.com>
2022-03-12 21:48:48 -0500

    replaced vh with javascript updated height'

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Navbar from '../components/Navbar'
 import Socials from '../components/Socials'
 import '../styles/layout.scss'
 
 export default function Layout({ children }) {
+  const [height, setHeight] = useState(window.innerHeight)
+
+  useEffect(() => {
+    window.addEventListener('resize', updateHeight);
+    return () => window.removeEventListener('resize', updateHeight);
+  });
+
+  const updateHeight = () => setHeight(window.innerHeight);
+
   return (
-    <div className='wrapper'>
+    <div className='wrapper' style={{minHeight: height}}>
       <Navbar/>
         { children }
       <Socials/>
     </div>
   )
 }
+
+// export default height;
