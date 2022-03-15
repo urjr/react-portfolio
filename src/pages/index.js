82428c4cdd118dfd575c67eb35a7a2e808090256
Ulises Reyes <urjr@gmail.com>
2022-03-15 14:49:19 -0400

    refined colors and animation for main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,13 @@ import Layout from '../layouts/Layout'
 import * as componentStyles from '../styles/main.module.scss'
 
 export default function Home() {
-    const introText = "Hi,-I'm-Ulises,-a-UX Designer-and-Illustrator-based-in-Brooklyn,-New York"
-    const introWords = introText.split("-")
+    let introText = "Hi, I'm Ulises, a UX-Designer and Illustrator based in Brooklyn, New-York"
+    introText = introText.split(" ")
+
+    const introWords = []
+
+    introText.map( word => { return introWords.push(word.replace('-', ' ')) })
+    console.log(introWords)
 
     return (
         <Layout>
@@ -12,13 +17,13 @@ export default function Home() {
                 <h1>
                     {introWords.map((word, index) => (
                         <span key={word} className={componentStyles.mask}>
-                            <span className={componentStyles.animate} style={{ animationDelay: `${(index * .1) + .25}s` }}>
-                                { word === "Ulises," || word === "UX Designer" || word === "Illustrator" || word === "Brooklyn," || word === "New York" ? <button className={componentStyles.hoverable}>{ word }</button> : word }&nbsp;<wbr/>
+                            <span className={componentStyles.animate} style={{ animationDelay: `${(index * .1) + .5}s` }}>
+                                { word === "Ulises," || word === "UX Designer" || word === "Illustrator" || word === "Brooklyn," || word === "New York" ? <a href='' className={componentStyles.hoverable}>{ word }</a> : word }&nbsp;<wbr/>
                             </span>
                         </span>
                     ))}
                 </h1>
-                <h2>Currently, I design Marketing Tools for Google. Formerly at Notarize, Broadlume, and Smarking</h2>
+                <h2>Currently, I design <a href='https://marketingplatform.google.com/about/' target="_blank" rel="noreferrer">Marketing Tools</a> for <a href='https://about.google/' target="_blank" rel="noreferrer">Google</a>. Formerly at <a href='https://notarize.com' target="_blank" rel="noreferrer">Notarize</a>, <a href='https://www.broadlume.com/' target="_blank" rel="noreferrer">Broadlume</a>, and <a href='https://www.smarking.com/' target="_blank" rel="noreferrer">Smarking</a></h2>
             </section>
         </Layout>
     )
